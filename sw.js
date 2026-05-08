c8e1b91c7666010e816602ba2ab33a11ab5cbc73
Romain Rivière <romain@levia.ai>
2026-05-08 17:52:39 +0200

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='hypernet-pwa-v10';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/data/levels.json','/assets/background.jpg','/assets/play.png'];
+const ASSETS=['/','/index.html','/manifest.webmanifest','/data/levels.json','/assets/music.mp3','/assets/background.jpg','/assets/play.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('hypernet-pwa-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})).catch(()=>caches.match('/')))});
